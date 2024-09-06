a209ddafcb3d63b08be6bb063447f6ce451b50f6
Adriano <jld.adriano@gmail.com>
2024-09-06 14:05:05 -0700

    lfake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A simple Python 3.12 project with Poetry";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python312
+            poetry
+          ];
+        };
+      });
+}
